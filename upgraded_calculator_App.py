cb5d300391868856c73f613c02d66193cdca6839
siphosetu mzayiya <siphosetumzayiya@siphosetus-MacBook-Air.local>
2024-03-08 18:05:06 +0200

    Initial commit

---
+++ upgraded_calculator_App.py
@@ -0,0 +1,14 @@
+Num1 = float(input("enter number here"))
+op = input("enter operator here")
+Num2 = float(input("Enter number here"))
+
+if op == "+":
+    print(Num1 + Num2)
+elif op == "-":
+    print(Num1 - Num2)
+elif op == "*":
+    print(Num1 * Num2)
+elif op == "/":
+    print(Num1 / Num2)
+else :
+    print("invalid error")
